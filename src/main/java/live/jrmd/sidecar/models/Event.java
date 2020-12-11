7e95c88b6f9759d5094c13fbf7c1f12d5d7a0c3b
Donelle Harris <donelle.harris@gmail.com>
2020-12-11 10:49:09 -0600

    Create Event & Motorcycle models
    Add constructors, getters & setters to Motorcycle model
    Instantiate Event model with appropriate columns, constructors, getters & setters

---
+++ src/main/java/live/jrmd/sidecar/models/Event.java
@@ -0,0 +1,101 @@
+package live.jrmd.sidecar.models;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "events")
+public class Event {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column
+    private String name;
+
+    @Column(nullable = false, columnDefinition = "TEXT")
+    private String description;
+
+    @Column(nullable = false)
+    private String date;
+
+    @Column(length = 10, nullable = false)
+    private String zipcode;
+
+    @Column(nullable = false)
+    private String eventType;
+
+    public Event(){}
+
+    public Event(Long id, String name, String description, String date, String zipcode, String eventType) {
+        this.id = id;
+        this.name = name;
+        this.description = description;
+        this.date = date;
+        this.zipcode = zipcode;
+        this.eventType = eventType;
+    }
+
+    public Event(String name, String description, String date, String zipcode, String eventType) {
+        this.name = name;
+        this.description = description;
+        this.date = date;
+        this.zipcode = zipcode;
+        this.eventType = eventType;
+    }
+
+    public Event (Event copy){
+        this.name = copy.name;
+        this.description = copy.description
+        this.date = copy.date;
+        this.zipcode = copy.zipcode;
+        this.eventType = copy.eventType;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public String getZipcode() {
+        return zipcode;
+    }
+
+    public void setZipcode(String zipcode) {
+        this.zipcode = zipcode;
+    }
+
+    public String getEventType() {
+        return eventType;
+    }
+
+    public void setEventType(String eventType) {
+        this.eventType = eventType;
+    }
+}
--- src/main/java/live/jrmd/sidecar/models/Motorcycle.java
+++ src/main/java/live/jrmd/sidecar/models/Motorcycle.java
@@ -24,5 +24,77 @@ public class Motorcycle {
     @Column(nullable = false)
     private String style;
 
+    public Motorcycle(){}
+    public Motorcycle(Long id, String year, String make, String model, String photo_url, String style) {
+        this.id = id;
+        this.year = year;
+        this.make = make;
+        this.model = model;
+        this.photo_url = photo_url;
+        this.style = style;
+    }
+    public Motorcycle(String year, String make, String model, String photo_url, String style) {
+        this.year = year;
+        this.make = make;
+        this.model = model;
+        this.photo_url = photo_url;
+        this.style = style;
+    }
+    public Motorcycle (Motorcycle copy){
+        this.id = copy.id;
+        this.make = copy.make;
+        this.model = copy.model;
+        this.year = copy.year;
+        this.style = copy.style;
+        this.photo_url = copy.photo_url;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getYear() {
+        return year;
+    }
+
+    public void setYear(String year) {
+        this.year = year;
+    }
+
+    public String getMake() {
+        return make;
+    }
+
+    public void setMake(String make) {
+        this.make = make;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public String getPhoto_url() {
+        return photo_url;
+    }
+
+    public void setPhoto_url(String photo_url) {
+        this.photo_url = photo_url;
+    }
+
+    public String getStyle() {
+        return style;
+    }
+
+    public void setStyle(String style) {
+        this.style = style;
+    }
 
 }
